5bfa08b40ecfbc0114a3f7cf6235a21d1e680a03
kenertj <tsoojkk@gmail.com>
2022-10-11 14:45:15 +0300

    Styling

--- src/Components/Edit.js
+++ src/Components/Edit.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from 'react-router-dom'
 import { Box, Typography, TextField, Button } from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2';
 
 
 const Edit = (props) => {
@@ -45,49 +46,141 @@ const Edit = (props) => {
 
 
     return (
-        <Box>
-            <Typography sx={{
-                textTransform: 'uppercase'
-            }}>
-                Shipment Details
-            </Typography>
-            <Typography sx={{
+        <Box sx={{
+
+        }}>
+            <Box sx={{
+                margin: '15vh auto',
+                maxWidth: '1100px',
+                background: '#f2f4f9',
 
             }}>
-                orderNo
-            </Typography>
-            <TextField id="filled-basic" variant="filled" value={orderNo} onChange={(e) => setorderNo(e.target.value)} />
-            <Typography sx={{
+                <Grid container spacing={2} sx={{
+                    ml: '2vh'
+                }}>
+                    <Grid xs={12}>
+                        <Typography sx={{
+                            textTransform: 'uppercase',
+                            mt: '5vh',
+                            ml: '3vh',
+                            fontSize: '0.75rem',
+                            letterSpacing: '2px',
+                            color: '#8898aa',
+                            fontWeight: '650'
+                        }}>
+                            Shipment Details
+                        </Typography>
+                    </Grid>
 
-            }}>
-                date
-            </Typography>
-            <TextField id="filled-basic" variant="filled" value={date} onChange={(e) => setDate(e.target.value)} />
-            <Typography sx={{
+                    <Grid xs={6}>
+                        <Typography sx={{
+                            color: '#525f7f',
+                            fontSize: '0.9rem',
+                            fontWeight: '600'
+                        }}>
+                            orderNo
+                        </Typography>
+                        <TextField id="filled-basic" variant="filled" value={orderNo} onChange={(e) => setorderNo(e.target.value)} sx={{
+                            width: '90%',
+                            input: {
+                                color: "#8898aa",
+                            }
+                        }} />
+                    </Grid>
+                    <Grid xs={6}>
+                        <Typography sx={{
+                            color: '#525f7f',
+                            fontSize: '0.9rem',
+                            fontWeight: '600'
+                        }}>
+                            date
+                        </Typography>
+                        <TextField id="filled-basic" variant="filled" value={date} onChange={(e) => setDate(e.target.value)} sx={{
+                            width: '90%',
+                            input: {
+                                color: "#8898aa",
+                            }
+                        }} />
+                    </Grid>
+                    <Grid xs={6}>
+                        <Typography sx={{
+                            color: '#525f7f',
+                            fontSize: '0.9rem',
+                            fontWeight: '600'
+                        }}>
+                            customer
+                        </Typography>
+                        <TextField id="filled-basic" variant="filled" value={customer} onChange={(e) => setCustomer(e.target.value)} sx={{
+                            width: '90%',
+                            input: {
+                                color: "#8898aa",
+                            }
+                        }} />
+                    </Grid>
+                    <Grid xs={6}>
+                        <Typography sx={{
+                            color: '#525f7f',
+                            fontSize: '0.9rem',
+                            fontWeight: '600'
+                        }}>
+                            trackingNo
+                        </Typography>
+                        <TextField id="filled-basic" variant="filled" value={trackingNo} onChange={(e) => setTrackingNo(e.target.value)} sx={{
+                            width: '90%',
+                            input: {
+                                color: "#8898aa",
+                            }
+                        }} />
+                    </Grid>
+                    <Grid xs={6}>
+                        <Typography sx={{
+                            color: '#525f7f',
+                            fontSize: '0.9rem',
+                            fontWeight: '600'
+                        }}>
+                            consignee
+                        </Typography>
+                        <TextField id="filled-basic" variant="filled" value={consignee} onChange={(e) => setConsignee(e.target.value)} sx={{
+                            width: '90%',
+                            input: {
+                                color: "#8898aa",
+                            }
+                        }} />
+                    </Grid>
+                    <Grid xs={6}>
+                        <Typography sx={{
+                            color: '#525f7f',
+                            fontSize: '0.9rem',
+                            fontWeight: '600'
+                        }}>
+                            status
+                        </Typography>
+                        <TextField id="filled-basic" variant="filled" value={status} onChange={(e) => setStatus(e.target.value)} sx={{
+                            width: '90%',
+                            input: {
+                                color: "#8898aa",
+                            },
 
-            }}>
-                customer
-            </Typography>
-            <TextField id="filled-basic" variant="filled" value={customer} onChange={(e) => setCustomer(e.target.value)} />
-            <Typography sx={{
 
-            }}>
-                trackingNo
-            </Typography>
-            <TextField id="filled-basic" variant="filled" value={trackingNo} onChange={(e) => setTrackingNo(e.target.value)} />
-            <Typography sx={{
+                        }} />
+                    </Grid>
+                    <Grid xs={12}>
+                        <Box sx={{
+                            margin: '0 auto',
+                            width: '550px'
+                        }}>
+                            <Button variant="contained" onClick={(e) => handleUpdate(e)} sx={{
+                                backgroundColor: '#11cdef',
+                                width: '100%',
+                                ml: '-2vh',
+                                mt: '2vh'
 
-            }}>
-                consignee
-            </Typography>
-            <TextField id="filled-basic" variant="filled" value={consignee} onChange={(e) => setConsignee(e.target.value)} />
-            <Typography sx={{
 
-            }}>
-                status
-            </Typography>
-            <TextField id="filled-basic" variant="filled" value={status} onChange={(e) => setStatus(e.target.value)} />
-            <Button variant="contained" onClick={(e) => handleUpdate(e)}>Update</Button>
+                            }}>Update</Button>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Box>
         </Box>
     )
 }
